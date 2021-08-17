7e79f858f5388de41694d98ccec487ea160b0f8b
truemaxevans <daorodney@gmail.com>
2021-08-17 21:47:37 +0300

    added one more route

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 # when upload on server set False
