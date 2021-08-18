e174a5728955f5d0d74f5b973cce9320185583f7
truemaxevans <daorodney@gmail.com>
2021-08-18 21:04:40 +0300

    SQL

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mystore.db"
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.Boolean, default=True)
 
 
 @app.route('/')
@@ -11,6 +22,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/add_product')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
