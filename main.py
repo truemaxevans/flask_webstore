4c4cf08b0dff69904a075f54ab61b38e16307832
truemaxevans <daorodney@gmail.com>
2021-08-22 23:26:00 +0300

    Info on page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,10 +14,16 @@ class Product(db.Model):
     description = db.Column(db.Text)
     status = db.Column(db.Boolean, default=True)
 
+    # show title for added positions
+    def __repr__(self):
+        return self.title
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Show all products ordered by price
+    products = Product.query.order_by(Product.price).all()
+    return render_template('index.html', data=products)
 
 
 @app.route('/about')
@@ -25,9 +31,23 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/add_product')
+@app.route('/add_product', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        description = request.form['description']
+
+        product = Product(title=title, price=price, description=description)
+        try:
+            db.session.add(product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error'
+
+    else:
+        return render_template('create.html')
 
 
 if __name__ == '__main__':
